28daa8f9048fc0b9800b4ac6256179c7518ed647
Stefan Fransen <stefan@flashpoint.nl>
2023-01-17 21:10:41 +0100

    :wip:

--- docker/build-config/env.php
+++ docker/build-config/env.php
@@ -1,121 +1,65 @@
 <?php
 return [
     'backend' => [
-        'frontName' => 'storemanager'
+        'frontName' => 'storemanager',
     ],
-    'remote_storage' => [
-        'driver' => 'file'
-    ],
-    'queue' => [
-        'consumers_wait_for_messages' => 1
-    ],
-    'crypt' => [
-        'key' => 'b096cc7d7e39290d9d87be024197f55b'
-    ],
-    'db' => [
-        'table_prefix' => '',
-        'connection' => [
-            'default' => [
-                'host' => 'localhost',
-                'dbname' => 'tropical_magento',
-                'username' => 'root',
-                'password' => 'Stefan230894',
-                'model' => 'mysql4',
-                'engine' => 'innodb',
-                'initStatements' => 'SET NAMES utf8;',
-                'active' => '1',
-                'driver_options' => [
-                    1014 => false
-                ]
-            ]
-        ]
+    'store_mapping' => [
+        'tropicalreefs_nl' => 'nl',
+        'tropicalreefs_en' => 'en',
     ],
     'resource' => [
         'default_setup' => [
-            'connection' => 'default'
-        ]
-    ],
-    'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'production',
-    'session' => [
-        'save' => 'redis',
-        'redis' => [
-            'host' => '127.0.0.1',
-            'port' => '6379',
-            'timeout' => '2.5',
-            'database' => '2',
-            'compression_threshold' => '0',
-            'compression_library' => 'gzip',
-            'log_level' => '3',
-            'max_concurrency' => '6',
-            'break_after_frontend' => '5',
-            'break_after_adminhtml' => '30',
-            'first_lifetime' => '600',
-            'bot_first_lifetime' => '60',
-            'bot_lifetime' => '7200',
-            'disable_locking' => '0',
-            'min_lifetime' => '60',
-            'max_lifetime' => '2592000'
-        ]
-    ],
-    'cache' => [
-        'frontend' => [
-            'default' => [
-                'id_prefix' => '77e_',
-                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
-                'backend_options' => [
-                    'server' => '127.0.0.1',
-                    'database' => '3',
-                    'port' => '6379',
-                    'password' => '',
-                    'compress_data' => '1',
-                    'compression_lib' => ''
-                ]
-            ],
-            'page_cache' => [
-                'id_prefix' => '77e_',
-                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
-                'backend_options' => [
-                    'server' => '127.0.0.1',
-                    'database' => '3',
-                    'port' => '6379',
-                    'password' => '',
-                    'compress_data' => '1',
-                    'compression_lib' => ''
-                ]
-            ]
+            'connection' => 'default',
         ],
-        'allow_parallel_generation' => false,
-        'graphql' => [
-            'id_salt' => 'HeEIXWPQyq4DjMQAPL4Wc2GP7ip0A6Z2'
-        ]
     ],
-    'lock' => [
-        'provider' => 'db'
-    ],
-    'directories' => [
-        'document_root_is_pub' => true
-    ],
+    'MAGE_MODE' => 'production',
     'cache_types' => [
-        'config' => 1,
-        'layout' => 1,
-        'block_html' => 1,
-        'collections' => 1,
-        'reflection' => 1,
-        'db_ddl' => 1,
-        'compiled_config' => 1,
-        'eav' => 1,
-        'customer_notification' => 1,
-        'config_integration' => 1,
-        'config_integration_api' => 1,
-        'full_page' => 1,
-        'config_webservice' => 1,
-        'translate' => 1
+        'config' => 0,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 0,
+        'reflection' => 0,
+        'db_ddl' => 0,
+        'compiled_config' => 0,
+        'eav' => 0,
+        'customer_notification' => 0,
+        'config_integration' => 0,
+        'config_integration_api' => 0,
+        'full_page' => 0,
+        'config_webservice' => 0,
+        'translate' => 0,
+    ],
+    'system' => [
+        'default' => [
+            'web' => [
+                'unsecure' => [
+                    'base_url' => 'https://dev.tropicalreefs.nl/',
+                    'base_link_url' => '{{unsecure_base_url}}',
+                    'base_static_url' => null,
+                    'base_media_url' => null,
+                ],
+                'secure' => [
+                    'base_url' => 'https://dev.tropicalreefs.nl/',
+                    'base_link_url' => '{{secure_base_url}}',
+                    'base_static_url' => null,
+                    'base_media_url' => null,
+                ],
+                'default' => [
+                    'front' => 'cms',
+                ],
+                'cookie' => [
+                    'cookie_path' => null,
+                    'cookie_domain' => null,
+                ],
+            ],
+            'admin' => [
+                'url' => [
+                    'custom' => null,
+                ],
+            ],
+        ],
     ],
     'downloadable_domains' => [
-        'dev.tropicalreefs.nl'
+        'dev.tropicalreefs.nl',
     ],
-    'install' => [
-        'date' => 'Mon, 07 Nov 2022 18:42:29 +0000'
-    ]
 ];
